6f0e0b76dc928f0efa2e260f1417577edf8a17cc
titoumess <titouan.messager@isen-ouest.yncrea.fr>
2025-05-13 15:28:32 +0200

    Initial commit - SalesTraction

---
+++ src/main/java/com/salestraction/ProjectSalestractionApplication.java
@@ -0,0 +1,13 @@
+package com.salestraction.sales_traction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSalestractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSalestractionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salestraction/sales_traction/ProjectSalestractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salestraction.sales_traction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSalestractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
